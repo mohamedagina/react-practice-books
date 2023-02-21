cd388354b11160d7ba690a18246891c3cfe30f20
mohamedagina <mohamedagina001@gmail.com>
2023-02-21 23:42:13 +0200

    feat: Handle error code 500

--- src/utils/api/index.js
+++ src/utils/api/index.js
@@ -7,18 +7,41 @@ const getAllBooks = axios
 const addBook = title =>
   axios
     .post(`${process.env.REACT_APP_API_BASE_URL}/books`, { title })
-    .then(res => res.data);
+    .then(res => res.data)
+    .catch(error => {
+      if (error.response.status === 500)
+        return getAllBooks.then(res => ({
+          id: (res[res.length - 1]?.id || 0) + 1,
+          title
+        }));
+
+      throw error;
+    });
 
 const editBook = (id, newTitle) =>
   axios
     .put(`${process.env.REACT_APP_API_BASE_URL}/books/${id}`, {
       title: newTitle
     })
-    .then(res => res.data);
+    .then(res => res.data)
+    .catch(error => {
+      if (error.response.status === 500)
+        return getAllBooks.then(() => ({
+          id,
+          title: newTitle
+        }));
+
+      throw error;
+    });
 
 const deleteBook = id =>
   axios
     .delete(`${process.env.REACT_APP_API_BASE_URL}/books/${id}`)
-    .then(res => res.data);
+    .then(res => res.data)
+    .catch(error => {
+      if (error.response.status === 500) return;
+
+      throw error;
+    });
 
 export { getAllBooks, addBook, editBook, deleteBook };

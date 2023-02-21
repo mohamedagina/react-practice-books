f7605ca2a2896611b84b0ad50595f75069e8cfe5
mohamedagina <mohamedagina001@gmail.com>
2023-02-21 18:29:23 +0200

    fix: Change env variable name

--- src/utils/api/index.js
+++ src/utils/api/index.js
@@ -1,18 +1,22 @@
 import axios from 'axios';
 
 const getAllBooks = axios
-  .get(`${process.env.BASE_URL}/books`)
+  .get(`${process.env.REACT_APP_API_BASE_URL}/books`)
   .then(res => res.data);
 
 const addBook = title =>
-  axios.post(`${process.env.BASE_URL}/books`, { title }).then(res => res.data);
+  axios
+    .post(`${process.env.REACT_APP_API_BASE_URL}/books`, { title })
+    .then(res => res.data);
 
 const editBook = (id, newTitle) =>
   axios
-    .put(`${process.env.BASE_URL}/books/${id}`, { newTitle })
+    .put(`${process.env.REACT_APP_API_BASE_URL}/books/${id}`, { newTitle })
     .then(res => res.data);
 
 const deleteBook = id =>
-  axios.delete(`${process.env.BASE_URL}/books/${id}`).then(res => res.data);
+  axios
+    .delete(`${process.env.REACT_APP_API_BASE_URL}/books/${id}`)
+    .then(res => res.data);
 
 export { getAllBooks, addBook, editBook, deleteBook };
